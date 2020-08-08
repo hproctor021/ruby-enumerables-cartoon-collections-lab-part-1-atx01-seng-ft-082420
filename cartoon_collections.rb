ff69b1965ecf92a4e6419bf788173b141aecf4ea
Haley Proctor <hproctor021@gmail.com>
2020-08-08 20:55:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def list_dwarves(array)
   total = 0
   array.each_with_index {|name, index|
-    total[name] = index }
+    array[name] = index }
     print index.to_s + "." + name
   end
   total += 1
